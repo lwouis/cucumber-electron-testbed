19ac6cdbf4a4c7a36876c32031a50f7f510cca8e
Louis Pontoise <louis.pontoise@amadeus.com>
2018-06-18 18:09:52 +0200

    Trying to ignore zone-testing and jasmine, and add before async function

--- test/features/step_definitions/steps.ts
+++ test/features/step_definitions/steps.ts
@@ -1,28 +1,27 @@
-import {Given, Then, When} from 'cucumber';
-import {TestBed} from '@angular/core/testing';
+import {Before, Then} from 'cucumber';
+import {getTestBed, TestBed} from '@angular/core/testing';
 import {AppComponent} from '../../../src/app/app.component';
 
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
-import 'zone.js/dist/zone-testing';
-import { getTestBed } from '@angular/core/testing';
-import {
+import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
+// var jasmine = 'jasmine';
+// import 'zone.js/dist/zone-testing';
+
+getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+  platformBrowserDynamicTesting()
+);
 
-Then('component X is instanciated', () => {
-  getTestBed().initTestEnvironment(
-    BrowserDynamicTestingModule,
-    platformBrowserDynamicTesting()
-  );
-
+Before(async function () {
   TestBed.configureTestingModule({
     declarations: [
       AppComponent,
     ],
   }).compileComponents();
+});
 
+Then('component X is instanciated', () => {
   const fixture = TestBed.createComponent(AppComponent);
   const app = fixture.debugElement.componentInstance;
   expect(app).toBeTruthy();
