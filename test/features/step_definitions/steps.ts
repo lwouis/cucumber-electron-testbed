0e06dbf9982ba0ef02e0ad5fd129893a3e91999b
Louis Pontoise <louis.pontoise@amadeus.com>
2018-06-18 17:45:42 +0200

    Adding stuff required by angular

--- test/features/step_definitions/steps.ts
+++ test/features/step_definitions/steps.ts
@@ -2,12 +2,27 @@ import {Given, Then, When} from 'cucumber';
 import {TestBed} from '@angular/core/testing';
 import {AppComponent} from '../../../src/app/app.component';
 
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/zone-testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
 Then('component X is instanciated', () => {
+  getTestBed().initTestEnvironment(
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
+  );
+
   TestBed.configureTestingModule({
     declarations: [
       AppComponent,
     ],
   }).compileComponents();
+
   const fixture = TestBed.createComponent(AppComponent);
   const app = fixture.debugElement.componentInstance;
   expect(app).toBeTruthy();
