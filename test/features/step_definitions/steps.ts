0d105b09a4e6247ef7a9d56756936f81dafc52e9
Matt Wynne <matt@mattwynne.net>
2018-07-11 15:44:53 +0100

    We made it work!!

--- test/features/step_definitions/steps.ts
+++ test/features/step_definitions/steps.ts
@@ -1,34 +1,42 @@
 import {After, Before, Then} from 'cucumber';
-import {getTestBed, TestBed} from '@angular/core/testing';
+import {getTestBed, TestBed, async} from '@angular/core/testing';
 import {AppComponent} from '../../../src/app/app.component';
+import { expect } from 'chai';
 
 import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
+import('core-js/es6/reflect');
+require('core-js/es7/reflect');
+require('zone.js/dist/zone.js');
+require('zone.js/dist/proxy.js');
+require('zone.js/dist/sync-test');
+require('zone.js/dist/async-test');
+require('zone.js/dist/fake-async-test');
+
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
-// var jasmine = 'jasmine';
-// import 'zone.js/dist/zone-testing';
 
-Before(() => {
-  getTestBed().initTestEnvironment(
-    BrowserDynamicTestingModule,
-    platformBrowserDynamicTesting(),
-  );
-});
+const ambientZone = Zone.current;
+const { ProxyZoneSpec } = Zone;
+const testProxyZone = ambientZone.fork(new ProxyZoneSpec());
 
-After(() => {
-  TestBed.resetTestingModule();
-});
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
 
-Before(async () => {
-  await TestBed.configureTestingModule({
-    declarations: [
-      AppComponent,
-    ],
-  }).compileComponents();
-});
+Before(async () =>
+  await testProxyZone.run(() =>
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+      ],
+    }).compileComponents()
+  )
+);
 
-Then('component X is instanciated', () => {
-  const fixture = TestBed.createComponent(AppComponent);
-  const app = fixture.debugElement.componentInstance;
-  expect(app).toBeTruthy();
+Then('component X is instanciated',() => {
+  testProxyZone.run(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).to.exist;
+  })
 });
